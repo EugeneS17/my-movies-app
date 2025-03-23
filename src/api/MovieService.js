3bbaf9838bea9324da0889a5fd1521a1f14307b6
Eugene Sosnovskiy <105513448+EugeneS17@users.noreply.github.com>
2025-03-23 15:36:42 +0300

    +Search & Pagination

--- src/api/MovieService.js
+++ src/api/MovieService.js
@@ -4,7 +4,6 @@ export default class MovieService {
   constructor() {
     this._apiUrl = 'https://api.themoviedb.org/3'
     this._posterUrl = 'https://image.tmdb.org/t/p/w500'
-    this._defaultPosterUrl = 'https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=IMAGE'
     this._apiToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWM3ZDAwMDY3MTA3OTZiNjhjNjczMWU2OGIxNWE2MiIsIm5iZiI6MTc0MDY3MTg5Ny4yNTgsInN1YiI6IjY3YzA4Yjk5ODM0MDU4ZjE2YWM4ZThkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.x9GWGHF0F19DIfIzdP-eHQd1O7abARibRc4DDVzXjc0'
     this._headers = {
@@ -22,18 +21,19 @@ export default class MovieService {
     return response.data
   }
 
-  async searchMovies(query, includeGenres = false) {
+  async searchMovies(query, page = 1) {
     if (!query) return
 
     try {
       const data = await this.fetchData('/search/movie', {
         query,
+        page,
       })
 
-      if (includeGenres) {
-        const genres = await this.getMovieGenres()
-        return this.convertMoviesData({ results: data.results, genres })
-      }
+      // if (includeGenres) {
+      // const genres = await this.getMovieGenres()
+      // return this.convertMoviesData({ results: data.results, genres })
+      // }
 
       return this.convertMoviesData(data)
     } catch (e) {
@@ -46,23 +46,27 @@ export default class MovieService {
     return data.genres
   }
 
-  convertMoviesData({ results, genres }) {
-    return results.map((movie) => {
-      const posterUrl = movie.poster_path && this._posterUrl + movie.poster_path
-      const movieGenres =
-        genres &&
-        movie.genre_ids.map((id) => {
-          return genres.find((genre) => genre.id === id)
-        })
+  convertMoviesData({ results, ...data }) {
+    return {
+      results: results.map((movie) => {
+        const posterUrl = movie.poster_path && this._posterUrl + movie.poster_path
+        // const movieGenres =
+        //   genres &&
+        //   movie.genre_ids.map((id) => {
+        //     return genres.find((genre) => genre.id === id)
+        //   })
 
-      return {
-        id: movie.id,
-        title: movie.title,
-        releaseDate: movie.release_date,
-        description: movie.overview,
-        genres: movieGenres,
-        posterImageUrl: posterUrl || this._defaultPosterUrl,
-      }
-    })
+        return {
+          id: movie.id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          description: movie.overview,
+          // genres: movieGenres,
+          posterImageUrl: posterUrl,
+        }
+      }),
+      page: data.page,
+      totalResults: data.total_results,
+    }
   }
 }
